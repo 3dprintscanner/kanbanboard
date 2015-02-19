664b54b7f6072f7c6c01e4fb34cd368a7a006079
Andreas Röhler <andreas.roehler@online.de>
2015-02-19 18:25:37 +0100

    newcard.py

--- newcard.py
+++ newcard.py
@@ -1,3 +1,6 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+
 import unittest
 import uuid
 
@@ -9,26 +12,48 @@ default_list_value = 'todo'
 number = 1
 
 def create_new_card(name):
-	global number
-	card['name'] = name
-	card['list'] = default_list_value
-	card['owner'] = 'nil'
-	card['id'] = number
-	cards[card['id']] = card
-	number +=1
+    global number
+    card['name'] = name
+    card['list'] = default_list_value
+    card['owner'] = 'nil'
+    card['id'] = number
+    cards[card['id']] = card
+    number +=1
 
 def assign_card(id,new_owner='default owner'):
-	cards[id]['owner'] = new_owner
+    cards[id]['owner'] = new_owner
 
 def move_card_assigned(id,owner,target_list='wip'):
-	cards[id]['list']=target_list
+    cards[id]['list']=target_list
 
 def finish_card(id,owner):
-	if cards[id]['owner'] == owner:
-		cards[id]['list'] = 'done'
-	else:
-		print('Error, wrong or no owner supplied')
+    if cards[id]['owner'] == owner:
+        cards[id]['list'] = 'done'
+    else:
+        print('Error, wrong or no owner supplied')
+
+## Tests
 
 # class MyTest(unittest.TestCase):
-# 	def test(self):
-#     	assert('name' in cards)
+#       def test(self):
+#       assert('name' in cards)
+
+def name_exists_test():
+    assert 'name' in cards[1]
+    # if 'name' in cards[1]:
+    # print("Erfolg")
+    # else:
+    # print("Fehler")
+
+def wrong_positiv_test():
+    assert not 'foo' in cards[1], "foo should not be a valid sybmol of cards[1]"
+
+print('newcard.py')
+
+# run tests only if not imported
+if __name__ == '__main__' :
+    create_new_card('firstcase')
+    wrong_positiv_test()
+    print("__name__ == '__main__'")
+    print(cards)
+    name_exists_test()
