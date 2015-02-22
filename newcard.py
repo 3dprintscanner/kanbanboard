d0855b3ad6b94a2c5832db80697d62cb3521d1ed
Andreas Röhler <andreas.roehler@online.de>
2015-02-22 18:22:56 +0100

    Description of task added

--- newcard.py
+++ newcard.py
@@ -1,6 +1,21 @@
 #! /usr/bin/env python
 # -*- coding: utf-8 -*-
 
+# Coding dojo task:
+
+# Your team needs a digital Kanban board urgently.
+
+# So create a board with columns for To Do, In Progress, Review and
+# Done.
+
+# New tasks are set in the column To Do and are moved to the next
+# right column, if there is not already reached the WIP limit.
+
+# Tasks has to be token by team members in the moment they move it to
+# In Progress.
+
+# Visualize the board as you like.
+
 import unittest
 import uuid
 
